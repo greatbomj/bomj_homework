2daa16a565df24157bab3498875f4ab39e2dc251
bomj <greatbomj@gmail.com>
2025-03-09 12:56:00 +0300

    выполнение 2-6 заданий

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,34 +1,33 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  const discriminant = b ** 2 - 4 * a * c; 
+	let arr = [];
+	const discriminant = b ** 2 - 4 * a * c;
 
-  if (discriminant < 0) {
-    return arr;
-  } else if (discriminant === 0) {
-    const root = -b / (2 * a);
-    arr.push(root);
-  } else {
-    const sqrtD = Math.sqrt(discriminant);
-    const root1 = (-b + sqrtD) / (2 * a);
-    const root2 = (-b - sqrtD) / (2 * a);
-    arr.push(root1, root2);
-  }
+	if (discriminant < 0) {
+		return arr;
+	} else if (discriminant === 0) {
+		const root = -b / (2 * a);
+		arr.push(root);
+	} else {
+		const sqrtD = Math.sqrt(discriminant);
+		const root1 = (-b + sqrtD) / (2 * a);
+		const root2 = (-b - sqrtD) / (2 * a);
+		arr.push(root1, root2);
+	}
 
-  return arr;
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  const monthlyPercent = (percent / 100) / 12;
+	const monthlyPercent = (percent / 100) / 12;
 
-  const loanBody = amount - contribution;
+	const loanBody = amount - contribution;
 
-  const monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / ((Math.pow(1 + monthlyPercent, countMonths)) - 1)));
+	const monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / ((Math.pow(1 + monthlyPercent, countMonths)) - 1)));
 
-  const totalAmount = monthlyPayment * countMonths;
+	const totalAmount = monthlyPayment * countMonths;
 
-  const roundedTotalAmount = Math.round(totalAmount * 100) / 100;
+	const roundedTotalAmount = Math.round(totalAmount * 100) / 100;
 
-  return roundedTotalAmount;
-}
+	return roundedTotalAmount;
--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,62 @@
-﻿
+﻿function parseCount(value) {
+    const parsedValue = Number.parseFloat(value);
+    if (isNaN(parsedValue)) {
+        throw new Error("Невалидное значение");
+    }
+    return parsedValue;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return parseFloat(area.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            }
+        };
+    }
+}
+
+console.log(validateCount("123"));
+console.log(validateCount("abc"));
+
+const triangle = getTriangle(3, 4, 5);
+console.log(triangle.perimeter); 
+console.log(triangle.area); 
+
+const invalidTriangle = getTriangle(1, 2, 3);
+console.log(invalidTriangle.area);
+console.log(invalidTriangle.perimeter); 
